18ac23371c9af12abc71f53cd4a91bb21d43d72e
Grant Collins <grantcollins03@gmail.com>
2022-12-29 10:19:32 -0500

    Commented out JS file

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-// Error messages
-const lengthError = document.getElementById('length-error');
-const letterError = document.getElementById('letter-error');
-const numberError = document.getElementById('number-error');
-const specialCharError = document.getElementById('special-char-error');
+// // Error messages
+// const lengthError = document.getElementById('length-error');
+// const letterError = document.getElementById('letter-error');
+// const numberError = document.getElementById('number-error');
+// const specialCharError = document.getElementById('special-char-error');
 
-// const fname = document.getElementById('f-name').value;
+// // const fname = document.getElementById('f-name').value;
 
 
-const submitBtn = document.getElementById('submit-btn');
+// const submitBtn = document.getElementById('submit-btn');
 
-submitBtn.addEventListener('click', function() {
-    const pwd = document.getElementById('password');
-    const pwdConfirm = document.getElementById('password-confrm');
-    const email = document.getElementById('email');
-    matchPassword(pwd.value, pwdConfirm.value);
-});
+// submitBtn.addEventListener('click', function() {
+//     const pwd = document.getElementById('password');
+//     const pwdConfirm = document.getElementById('password-confrm');
+//     const email = document.getElementById('email');
+//     matchPassword(pwd.value, pwdConfirm.value);
+// });
 
-function matchPassword(str1, str2) {
-    const noMatchError = document.getElementById('no-match-error');
-    if (str1 !== str2) {
-        noMatchError.value = '<span>*</span>Passwords do not match';
-    }
-}
+// function matchPassword(str1, str2) {
+//     const noMatchError = document.getElementById('no-match-error');
+//     if (str1 !== str2) {
+//         noMatchError.value = '<span>*</span>Passwords do not match';
+//     }
+// }
